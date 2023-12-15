e416248a3d698f707c26f9919895b210a33e2aab
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-12-15 02:54:49 +0200

    clean code

--- funds/src/app/components/search/search.component.ts
+++ funds/src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnChanges, ViewChild } from '@angular/core';
 import { FundService } from 'src/app/services/fund.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,12 +12,42 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     standalone: true,
     imports: [MatFormFieldModule, MatInputModule, NgIf, MatButtonModule, MatIconModule, NgFor]
 })
-export class SearchComponent {
+export class SearchComponent implements OnChanges {
   searchQuery: string[] = [];
   showCloseIcon: boolean = false;
   @ViewChild('searchInput') searchInputEl: ElementRef;
 
   constructor(private fundService: FundService) { }
+
+  ngOnInit(): void {
+    this.subscribeToFavouriteButtonClicked();
+    this.subscribeToAllButtonClicked();  
+  }
+
+  ngOnChanges(): void {
+    this.subscribeToFavouriteButtonClicked();
+    this.subscribeToAllButtonClicked();  
+  }
+
+  // Display funds that are in favourites
+  subscribeToFavouriteButtonClicked(): void {
+    this.fundService.isFavourite$.subscribe((isFavourite) => {
+      if (isFavourite) {
+       this.searchQuery = [];
+       this.searchInputEl.nativeElement.value = '';
+      }
+    });
+  }
+
+  // Display all funds
+  subscribeToAllButtonClicked(): void {
+    this.fundService.isAll$.subscribe((isAll) => {
+      if (isAll) {
+       this.searchQuery = [];
+        this.searchInputEl.nativeElement.value = '';
+      }
+    });
+  }
 
   // Update search query and emit to subscribers
   updateSearch() {
